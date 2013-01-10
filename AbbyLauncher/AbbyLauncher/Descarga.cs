562b34cd441d971139921c28d9372e4cb7716fe9
crymore3@gmail.com <crymore3@gmail.com@86145bcc-2932-641b-40bf-8db704073500>
2013-01-10 06:39:52 +0000

    AbbyLauncher y AbbyPatchBuilder, solo me falta agregar que funcione sobre SerialKey

---
+++ AbbyLauncher/AbbyLauncher/Descarga.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AbbyLauncher
+{
+    public class Descarga
+    {
+        private WebClient cliente;
+        public int porcenaje;
+
+        public void descargar(string url, string archivo)
+        {
+            cliente = new WebClient();
+            cliente.DownloadProgressChanged += new DownloadProgressChangedEventHandler(cliente_progresschanged);
+            cliente.DownloadFileCompleted += new System.ComponentModel.AsyncCompletedEventHandler(cliente_descargacompleta);
+            string descarga = url + archivo;
+            if (archivo.ToLower().Equals("./" + Path.GetFileName(Application.ExecutablePath).ToLower()))
+                archivo += "_";
+            cliente.DownloadFileAsync(new Uri(descarga), archivo);
+            porcenaje = 0;
+        }
+
+        public int getPorcentaje()
+        {
+            return porcenaje;
+        }
+
+        private void cliente_progresschanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            double bytesIn = double.Parse(e.BytesReceived.ToString());
+            double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+            double percentage = bytesIn / totalBytes * 100;
+            porcenaje = int.Parse(Math.Truncate(percentage).ToString());
+        }
+
+        private void cliente_descargacompleta(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            porcenaje = 100;
+        }
+
+    }
+}
---
+++ AbbyLauncher/AbbyLauncher/XML.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace AbbyLauncher
+{
+    public class XML
+    {
+        private List<ITEM> lista = new List<ITEM>();
+        public List<ITEM> getArchivos(string archivo)
+        {
+            ITEM item;
+
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(archivo);
+
+            XmlNodeList FILES = xDoc.SelectNodes("//ITEM");
+            foreach (XmlNode nodos in FILES)
+            {
+                XmlElement nodo = (XmlElement)nodos;
+                item = new ITEM( nodo.GetAttribute("nombre"), (nodos.InnerText).ToUpper() );
+
+                lista.Add(item);
+            }
+
+            return lista;
+        }
+
+        
+    }
+}
